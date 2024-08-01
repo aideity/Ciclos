67dbecd665385685ca13ab265ade694475b58bf6
aideity <luchesimartina@gmail.com>
2024-08-01 13:56:39 -0300

    Add files via upload

---
+++ micro1.js
@@ -0,0 +1,7 @@
+let movimientos = [400000, -23000, 3400,-1000];
+
+
+Estimada Gloria Medina:
+El monto total de los depósitos es de: $5500.
+El monto total de los retiros es de: $15000.
+Por lo tanto, su saldo actual en la cuenta es de: $40000.
